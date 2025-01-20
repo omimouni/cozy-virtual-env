61fb3a3bb97e1fba295d3f893fe1e30fecd2c1cf
Oumar Mimouni <oumar.mimouni@gmail.com>
2025-01-20 20:50:54 +0100

    moved sounds to the public directory

--- src/config/sounds.js
+++ src/config/sounds.js
@@ -2,25 +2,25 @@ export const sounds = [
   {
     id: 'rain',
     name: 'Rain',
-    file: '/src/assets/rain.mp3',
+    file: '/rain.mp3',
     icon: 'faCloudRain',
   },
   {
     id: 'thunder',
     name: 'Thunder',
-    file: '/src/assets/thunder.mp3',
+    file: '/thunder.mp3',
     icon: 'faBolt',
   },
   {
     id: 'guitar',
     name: 'Guitar',
-    file: '/src/assets/guitar.mp3',
+    file: '/guitar.mp3',
     icon: 'faGuitar',
   },
   {
     id: 'piano',
     name: 'Piano',
-    file: '/src/assets/piano.mp3',
+    file: '/piano.mp3',
     icon: 'faKeyboard',
   }
 ]; 
